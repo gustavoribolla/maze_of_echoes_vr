0bb33bb86e38b388b601172ce343d4ff94367ed8
ribollequis87 <gustavocr2@al.insper.edu.br>
2026-05-13 09:12:56 -0300

    feat: projeto VR inicial

---
+++ Assets/AnimateHandOnInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty triggerValue;
+    public InputActionProperty gripValue;
+
+    public Animator handAnimator;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float trigger = triggerValue.action.ReadValue<float>();
+        float grip = gripValue.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Trigger", trigger);
+        handAnimator.SetFloat("Grip", grip);
+    }
+}
